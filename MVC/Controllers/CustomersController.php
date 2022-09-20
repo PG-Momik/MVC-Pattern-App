b238d699616bfc224e97adb549e7c071ee42119c
PG-Momik <momik12339@gmail.com>
2022-09-20 13:35:35 +0545

    Fix CustomerController

--- MVC/Controllers/CustomersController.php
+++ MVC/Controllers/CustomersController.php
@@ -1,46 +1,92 @@
 <?php
-{
-
-
-}
-
 
 require_once 'Models/Customer.php';
+require_once 'Misc/Helper.php';
+require_once 'Misc/Routes.php';
 
 class CustomersController
 {
-    public string $base = "MVC";
-    public function index():void
+    public function index(): void
     {
-        $data = (new Customer())->all();
-        if (session_status() === PHP_SESSION_NONE) {
-            session_start();
-        }
+        $datas = (new Customer())->all();
+
         include_once 'Views/Customers.php';
     }
 
-    public function registerView():void
+    public function register($data = '', $method = "GET"): void
     {
+        if ($method != "GET") {
+            session_suru();
+            $customer = new Customer();
+            $customer->name = $_POST['name'];
+            $customer->email = $_POST['email'];
+            $customer->address = $_POST['address'];
+            $customer->password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+            if ($this->validate(
+                    $customer->name,
+                    $customer->email,
+                    $customer->address,
+                    $customer->password
+                ) and $customer->insert()) {
+                $_SESSION['flashType'] = "success";
+                $_SESSION['flashMsg'] = "Registration successful.";
+            } else {
+                $_SESSION['flashType'] = "danger";
+                $_SESSION['flashMsg'] = "Registration unsuccessful";
+            }
+        }
+
         include_once 'Views/Register.php';
     }
 
-    public function register($data):void
+    public function login($data = '', $method = "GET"): void
     {
-        if (session_status() === PHP_SESSION_NONE) {
-            session_start();
+        if ($method != "GET") {
+            $isValid = $this->validate($_POST['email'], $_POST['password']);
+            if ($isValid) {
+                $customer = new Customer();
+                $customer->email = $_POST['email'];
+                $customer->password = $_POST['password'];
+                $temp = $customer->getByEmail();
+                if (password_verify($customer->password, $temp->password)) {
+                    session_suru();
+                    $_SESSION['userId'] = $temp->id;
+                    $_SESSION['isLoggedIn'] = true;
+                    $_SESSION['flashType'] = "success";
+                    $_SESSION['flashMsg'] = "Login successful";
+                    header('location:' . Routes::BASE . Routes::PROFILE);
+                }
+                $_SESSION['flashType'] = "danger";
+                $_SESSION['danger'] = "Login unsuccessful";
+            }
         }
+        include_once 'Views/Login.php';
+    }
 
+    public function profile(): void
+    {
+        session_suru();
         $customer = new Customer();
-        $customer->name = $_POST['name'];
-        $customer->email = $_POST['email'];
-        $customer->address = $_POST['address'];
-        $customer->password = $_POST['password'];
-        if ($customer->insert()) {
-            $_SESSION['success'] = "Registration successful.";
-        } else {
-            $_SESSION['fail'] = "Registration successful";
-        }
+        $customer->id = $_SESSION['userId'];
+        $data = $customer->single();
+        include_once 'Views/Profile.php';
+    }
 
-        $this->registerView();
+    private function validate(...$datas): bool
+    {
+        foreach ($datas as $index => $data) {
+            if (empty($data)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public function logout()
+    {
+        session_suru();
+        unset($_SESSION['isLoggedIn']);
+        unset($_SESSION['userId']);
+        header("location:/MVC/");
     }
 }
