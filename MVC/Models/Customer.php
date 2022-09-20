e136f59b841f34ce4c464bdc17bd7c05952cb649
PG-Momik <momik12339@gmail.com>
2022-09-20 13:37:33 +0545

    Add login()

--- MVC/Models/Customer.php
+++ MVC/Models/Customer.php
@@ -13,6 +13,7 @@ class Customer extends Database
     }
 
     public function insert():bool{
+
         $query = "INSERT INTO $this->table (name, email, address, password) VALUES (:name, :email, :address, :password)";
         $stmt = $this->conn->prepare($query);
         $params = array(
@@ -22,6 +23,15 @@ class Customer extends Database
             ":password" => $this->password
         );
         return $stmt->execute($params)??false;
+    }
+    public function getByEmail(){
+        $query = "SELECT * from $this->table WHERE email = :email";
+        $stmt = $this->conn->prepare($query);
+        $params = array(
+            'email'=>$this->email,
+        );
+        $stmt->execute($params);
+        return $stmt->fetch(PDO::FETCH_OBJ);
     }
 
 }
