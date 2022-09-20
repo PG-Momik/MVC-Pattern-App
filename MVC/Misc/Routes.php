855574e45e67907224bec99e3608b7b6473f5d87
PG-Momik <momik12339@gmail.com>
2022-09-20 13:36:58 +0545

    Add Route lookup table

--- MVC/Misc/Routes.php
+++ MVC/Misc/Routes.php
@@ -1,5 +1,12 @@
 <?php
 
 class Routes{
-    public const HOME = 'home';
+    public const BASE = "/MVC/";
+    public const HOME = "home";
+    public const ABOUT = "aboutus";
+    public const CUSTOMERS = "customers";
+    public const REGISTER = "customers/register";
+    public const LOGIN = "customers/login";
+    const PROFILE = "customers/profile";
+    const LOGOUT = "customers/logout";
 }
