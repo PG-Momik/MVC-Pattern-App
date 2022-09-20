71152f13b20e782d43de2178276467a9d8f76277
PG-Momik <momik12339@gmail.com>
2022-09-20 13:41:04 +0545

    Nav partial

--- MVC/Views/partials/_nav.php
+++ MVC/Views/partials/_nav.php
@@ -1,9 +1,24 @@
 <?php
-const BASE="/MVC"
+
+require_once 'Misc/Helper.php';
+require_once 'Misc/Routes.php';
+
+session_suru();
+
 ?>
-<nav>
-    <a href="<?=BASE?>/">Home</a>
-    <a href="<?=BASE?>/aboutus">About Us</a>
-    <a href="<?=BASE?>/customers">All customers</a>
-    <a href="<?=BASE?>/customers/register">Register</a>
+<nav class="navbar">
+    <a href="<?= Routes::BASE . Routes::HOME ?>" class="nav-link">Home</a>
+    <a href="<?= Routes::BASE . Routes::ABOUT ?>" class="nav-link">About Us</a>
+    <a href="<?= Routes::BASE . Routes::CUSTOMERS ?>" class="nav-link">All customers</a>
+    <?php
+    if (isset($_SESSION['isLoggedIn'])) { ?>
+        <a href="<?= Routes::BASE . Routes::PROFILE ?>" class="nav-link">Profile</a>
+        <a href="<?= Routes::BASE . Routes::LOGOUT ?>" class="nav-link">Logout</a>
+        <?php
+    } else { ?>
+        <a href="<?= Routes::BASE . Routes::LOGIN ?>" class="nav-link">Login</a>
+        <a href="<?= Routes::BASE . Routes::REGISTER ?>" class="nav-link">Register</a>
+        <?php
+    }
+    ?>
 </nav>
