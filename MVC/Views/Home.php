bfa13c912dc64dd4618640ba30dfa6bdfc35f531
PG-Momik <momik12339@gmail.com>
2022-09-20 13:39:01 +0545

    Fix home

--- MVC/Views/Home.php
+++ MVC/Views/Home.php
@@ -3,11 +3,14 @@
 <html>
 <head>
     <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 </head>
-<body>
-    <?php include_once 'partials/_nav.php'?>
+
+<?php include_once 'partials/_nav.php' ?>
+<body class="container text-center">
     <h1>Home</h1>
-    <p>This is home page.</p>
+    <p>This is Home page.</p>
 
 </body>
 </html> 
