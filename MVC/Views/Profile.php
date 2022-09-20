488bc2cfb65d6e397ce58f04381a4f532b0012d3
PG-Momik <momik12339@gmail.com>
2022-09-20 13:42:27 +0545

    Add profile

---
+++ MVC/Views/Profile.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once 'Misc/Helper.php';
+
+session_suru();
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+</head>
+
+<?php include_once 'partials/_nav.php' ?>
+<body class="container text-center">
+<h2>
+    Name:<?php $data->name ?>
+</h2>
+<table class="table table-hover">
+    <thead class="table-dark">
+        <th>Name</th>
+        <th>Email</th>
+        <th>Address</th>
+    </thead>
+    <tr>
+        <td><?= $data->name ?></td>
+        <td><?= $data->email ?></td>
+        <td><?= $data->address ?></td>
+    </tr>
+</table>
+
+</body>
+</html> 
