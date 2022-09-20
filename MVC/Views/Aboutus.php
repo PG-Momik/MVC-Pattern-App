6c708a50521897bcf2df98ef74e19d9f25452a9f
PG-Momik <momik12339@gmail.com>
2022-09-20 13:37:56 +0545

    Fix Aboutus

--- MVC/Views/Aboutus.php
+++ MVC/Views/Aboutus.php
@@ -1,16 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Home</title>
+    <title>About Us</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 </head>
-<body>
-<?php include_once 'partials/_nav.php'?>
+
+<?php include_once 'partials/_nav.php' ?>
+<body class="container text-center">
 <h1>About us</h1>
-<p>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, consequatur consequuntur cupiditate dignissimos
-    doloribus eligendi, enim et iste mollitia nesciunt officia optio pariatur, placeat rerum totam! Facilis molestias
-    placeat repellat!
-</p>
+<p>This is About us page.</p>
 
 </body>
 </html> 
