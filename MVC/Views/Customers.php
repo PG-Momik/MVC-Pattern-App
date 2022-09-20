fe9b424a0e165633aa94146bab4dc7c21f2fdf03
PG-Momik <momik12339@gmail.com>
2022-09-20 13:38:37 +0545

    Fix all customers

--- MVC/Views/Customers.php
+++ MVC/Views/Customers.php
@@ -1,10 +1,27 @@
+<!DOCTYPE html>
 <html>
 <head>
     <title>Customers</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 </head>
-<body>
-<?php include_once 'partials/_nav.php'?>
-<h2>Customers</h2>
-<?php print_r($data)?>
+<?php include_once 'partials/_nav.php' ?>
+<body class="container text-center">
+<h1>All Customers</h1>
+<table class="table table-hover">
+    <thead class="table-dark">
+        <th>Name</th>
+        <th>Email</th>
+        <th>Address</th>
+    </thead>
+    <?php foreach ($datas as $index => $data) { ?>
+        <tr>
+            <td><?= $data->name ?></td>
+            <td><?= $data->email ?></td>
+            <td><?= $data->address ?></td>
+        </tr>
+    <?php } ?>
+</table>
+
 </body>
-</html>
+</html> 
