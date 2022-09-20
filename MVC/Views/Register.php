56a472e7a781633542b875eed2ceb5f32d75b7a2
PG-Momik <momik12339@gmail.com>
2022-09-20 13:40:41 +0545

    Better session_flash()

--- MVC/Views/Register.php
+++ MVC/Views/Register.php
@@ -1,32 +1,43 @@
 <?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-$message = '';
-if(isset($_SESSION['success'])){
-    $message = $_SESSION['success'];
-}elseif ($_SESSION['danger']){
-    $message = $_SESSION['danger'];
-}
-unset($_SESSION['success'])
+require_once 'Misc/Helper.php';
+
+session_suru();
 
 ?>
 <html>
-<head><title>Register</title></head>
-<body>
-<?php include_once 'partials/_nav.php'?>
-<h2><?=$message?></h2>
-<form method="POST" action="<?=BASE?>/customers/register">
-    <input type="text" name="name">
-    <br>
-    <input type="text" name="email">
-    <br>
-    <input type="text" name="address">
-    <br>
-    <input type="text" name="password">
-    <br>
-    <input type="submit" value="Register">
-</form>
+<head>
+    <title>Register</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+                                 integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+</head>
+
+<?php include_once 'partials/_nav.php' ?>
+<body class="container text-center">
+<h2>
+    Register
+</h2>
+<?php session_flash(); ?>
+<div class="row">
+    <div class="col-2"></div>
+    <div class="col-8">
+        <form method="POST" action="<?php Routes::REGISTER?>">
+            <input type="text" class="form-control" name="name">
+            <br>
+            <input type="email" class="form-control" name="email">
+            <br>
+            <input type="text" class="form-control" name="address">
+            <br>
+            <input type="password" class="form-control" name="password">
+            <br>
+            <input type="submit" class="form-control btn btn-primary" value="Register">
+        </form>
+        <div class="col-2"></div>
+    </div>
+</div>
 </body>
-<?php include_once 'partials/_footer.php'?>
+<?php
+include_once 'partials/_footer.php' ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8"
+        crossorigin="anonymous"></script>
 </html>
